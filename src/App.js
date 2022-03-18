020d41ea860b94175eda0665d85553d610e1f8bd
Julien-desjardin <desjardinjulien59@gmail.com>
2022-03-17 19:46:58 -0600

    Initialization of the app with components and containers

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {Footer, Blog, Possibility, Features, WhatPortfolio, Header} from './containers';
+import {CTA, Brand, Navbar} from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatPortfolio />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+        </div>
+  )
+}
+
+export default App
